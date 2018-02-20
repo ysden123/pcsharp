3492aa414a6edcb432447ec17a8c1c5d5bdaa488
Yuriy Stul <yuriy.s@dauup-clicksmob.com>
2018-02-20 16:07:42 +0200

    App.config

---
+++ PCSharpBasics/Config/ConfigEx1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace PCSharpBasics.Config
+{
+    public class ConfigEx1
+    {
+        public string DefineSetting1()
+        {
+            return ConfigurationManager.AppSettings.Get("setting1");
+        }
+    }
+}
---
+++ UnitTestPCSharpBasics/Config/ConfigEx1Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PCSharpBasics.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCSharpBasics.Config.Tests
+{
+    [TestClass()]
+    public class ConfigEx1Tests
+    {
+        [TestMethod()]
+        public void DefineSetting1Test()
+        {
+            var conf = new ConfigEx1();
+            var v = conf.DefineSetting1();
+            Assert.AreNotEqual(conf.DefineSetting1(), null);
+            Assert.AreEqual<String>("value for setting1", conf.DefineSetting1());
+        }
+    }
+}
