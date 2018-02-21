b9f70927c443712cebfaf725b3162877b1fd3cf9
Yuriy Stul <yuriy.s@dauup-clicksmob.com>
2018-02-21 16:09:52 +0200

    async

---
+++ PCSharpBasics/MultiThreading/AsyncEx1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StulSoft.PCSharpBasics.MultiThreading
+{
+    /// <summary>
+    /// Playing with async and await
+    /// </summary>
+    public class AsyncEx1
+    {
+        /// <summary>
+        /// Some long process
+        /// </summary>
+        /// <returns>result</returns>
+        public async Task<int> LongProcess1()
+        {
+            Console.WriteLine($"==>LongProcess1 ({Thread.CurrentThread.ManagedThreadId})");
+            int result = await Task.Run(() =>
+            {
+                Console.WriteLine($"Running long process 1 ({Thread.CurrentThread.ManagedThreadId})");
+                // Compute total count of digits in strings.
+                int size = 0;
+                for (int z = 0; z < 100; z++)
+                {
+                    for (int i = 0; i < 1000000; i++)
+                    {
+                        string value = i.ToString();
+                        if (value == null)
+                        {
+                            return 0;
+                        }
+                        size += value.Length;
+                    }
+                }
+                Console.WriteLine($"Completed long process 1 ({Thread.CurrentThread.ManagedThreadId})");
+                return size;
+            });
+            Console.WriteLine($"<==LongProcess1 ({Thread.CurrentThread.ManagedThreadId})");
+            return result;
+        }
+
+        /// <summary>
+        /// Executes a long process 1.
+        /// </summary>
+        public void Test1()
+        {
+            Console.WriteLine($"==>Test1 ({Thread.CurrentThread.ManagedThreadId})");
+
+            Console.WriteLine($"Call LongProcess1 ({Thread.CurrentThread.ManagedThreadId})");
+            var result = LongProcess1();
+            Console.WriteLine($"Do some actions inside Test1 method ({Thread.CurrentThread.ManagedThreadId})");
+            Console.WriteLine($"Waiting...  ({Thread.CurrentThread.ManagedThreadId})");
+            result.Wait();
+            Console.WriteLine($"Result is {result.Result}  ({Thread.CurrentThread.ManagedThreadId})");
+            Console.WriteLine($"<==Test1  ({Thread.CurrentThread.ManagedThreadId})");
+        }
+    }
+}
---
+++ UnitTestPCSharpBasics/MultiThreading/AsyncEx1Tests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StulSoft.PCSharpBasics.MultiThreading.Tests
+{
+    [TestClass]
+    public class AsyncEx1Tests
+    {
+
+        [TestMethod]
+        public void Test1Test()
+        {
+            var mt = new AsyncEx1();
+            mt.Test1();
+        }
+    }
+}
