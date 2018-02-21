6cd5d02b82a7afa82aad032fe7769dd4f197006f
Yuriy Stul <yuriy.s@dauup-clicksmob.com>
2018-02-21 09:45:08 +0200

    Name spaces

---
+++ PCSharpBasics/NameSpaces/NMSpaceTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StulSoft.PCSharpBasics.NameSpaces
+{
+    /// <summary>
+    /// Playing with namespace
+    /// </summary>
+    public class NMSpaceTest1
+    {
+        public string Name { get; set; }
+        public NMSpaceTest1(string name)
+        {
+            Name = name;
+        }
+
+        public string Test1()
+        {
+            return Name;
+        }
+    }
+}
---
+++ UnitTestPCSharpBasics/NameSpaces/NMSpaceTest1Tests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StulSoft.PCSharpBasics.NameSpaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StulSoft.PCSharpBasics.NameSpaces.Tests
+{
+    [TestClass()]
+    public class NMSpaceTest1Tests
+    {
+        [TestMethod()]
+        public void NMSpaceTest1Test()
+        {
+            Assert.AreNotEqual(null, new NMSpaceTest1("test"));
+        }
+
+        [TestMethod()]
+        public void Test1Test()
+        {
+            var tmp = new NMSpaceTest1("test");
+            Assert.AreEqual<string>("test", tmp.Test1());
+        }
+    }
+}
