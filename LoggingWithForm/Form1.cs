780338fb8bdd17d4640629f463909754e5363148
ysden123 <ysden123@gmail.com>
2018-02-22 19:20:30 +0200

    Logging with Win Form application

---
+++ LoggingWithForm/Form1.cs
@@ -0,0 +1,23 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StulSoft.PCSharp.LoggingWithForm
+{
+    public partial class Form1 : Form
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Form1));
+        public Form1()
+        {
+            log.Info("Ctor of Form1");
+            InitializeComponent();
+        }
+    }
+}
---
+++ LoggingWithForm/Program.cs
@@ -0,0 +1,29 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StulSoft.PCSharp.LoggingWithForm
+{
+    static class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Load config
+            log4net.Config.XmlConfigurator.Configure();
+            
+            log.Info("Info");
+            log.Debug("Debug");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
