b2811c96415cf99b51adaa2cb6ce36991c7006db
ysden123 <ysden123@gmail.com>
2018-02-24 20:29:36 +0200

    linq

---
+++ PCSharpBasics/PLinq/LinqEx1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StulSoft.PCSharpBasics.PLinq
+{
+    public static class LinqEx1
+    {
+        public static int[] ConvertStringsToIntsAndSortIt()
+        {
+            var strings = new string[] { "0042", "17", "52" };
+            return strings.Select(s => Int32.Parse(s)).OrderBy(i => i).ToArray();
+        }
+    }
+}
---
+++ UnitTestPCSharpBasics/PLinq/LinqEx1Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StulSoft.PCSharpBasics.PLinq;
+
+namespace StulSoft.UnitTestPCSharpBasics.PLinq
+{
+    [TestClass]
+    public class LinqEx1Test
+    {
+        [TestMethod]
+        public void TestConvertStringsToIntsAndSortIt()
+        {
+            var result = LinqEx1.ConvertStringsToIntsAndSortIt();
+            Assert.IsTrue(result[0] == 17);
+            Assert.IsTrue(result[1] == 42);
+            Assert.IsTrue(result[2] == 52);
+        }
+    }
+}
