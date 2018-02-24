64a62ea365e35a76a0badbb59259a6e5187d5b92
ysden123 <ysden123@gmail.com>
2018-02-24 16:07:27 +0200

    Use the "out" argument

---
+++ PCSharpBasics/OutArgument/OutArgEx1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StulSoft.PCSharpBasics.OutArgument
+{
+    /// <summary>
+    /// Playing with the out argument
+    /// </summary>
+    public static class OutArgEx1
+    {
+        public static bool Method1(int i, out string text)
+        {
+            text = String.Format("i={0}", i);
+            return true;
+        }
+    }
+}
---
+++ UnitTestPCSharpBasics/OutArgument/OutArgEx1Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StulSoft.PCSharpBasics.OutArgument;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StulSoft.PCSharpBasics.OutArgument.Tests
+{
+    [TestClass()]
+    public class OutArgEx1Tests
+    {
+        [TestMethod()]
+        public void Method1Test()
+        {
+            var text = "";
+            var result = OutArgEx1.Method1(123, out text);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual<string>("i=123", text);
+        }
+    }
+}
