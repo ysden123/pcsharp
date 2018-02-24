7370dd66582d25f8f78ad977b5c52ddbcc47b71a
ysden123 <ysden123@gmail.com>
2018-02-24 17:38:00 +0200

    HelloLinq

---
+++ PCSharpBasics/PLinq/HelloLinq.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StulSoft.PCSharpBasics.PLinq
+{
+    public static class HelloLinq
+    {
+        public static IList<string> GetGreetings()
+        {
+            var greetings = new string[]{ "hello world", "hello LINQ", "hello Apress" };
+            var items =
+                from greeting in greetings
+                where greeting.EndsWith("LINQ")
+                select greeting;
+            return items.ToList();
+        }
+    }
+}
---
+++ UnitTestPCSharpBasics/PLinq/HelloLinqTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StulSoft.PCSharpBasics.PLinq;
+
+namespace StulSoft.UnitTestPCSharpBasics.PLinq
+{
+    [TestClass]
+    public class HelloLinqTest
+    {
+        [TestMethod]
+        public void TestGetGreetings()
+        {
+            var result = HelloLinq.GetGreetings();
+            Assert.IsTrue(result.Count == 1);
+        }
+    }
+}
